b58f9fc37f337967c8ccbc306c758b70b122769a
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2023-10-01 14:13:37 +0600

    Update soldier_and_banana.cpp

--- Lab-06/soldier_and_banana.cpp
+++ Lab-06/soldier_and_banana.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int k,n,w;
+	cin>>k>>n>>w;
+	int sum=0;
+	for(int i=0;i<=w;i++)
+	{
+		sum+=i;
 
+	}
+	int d=(sum*k)-n;
+	if (d<0)
+		cout<<"0";
+	else
+		cout<<d;
+}
